8f370e5d965784e28e4dd7e49e4d4ddeae5c5c90
Mikhail Krasikov <corolish2@gmail.com>
2021-03-01 14:09:20 +0300

    v2

---
+++ src/main/java/ru/mkrasikoff/springmvcapp/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package ru.mkrasikoff.springmvcapp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
