87aacb0511076fa4a4b4d028896f12499ee23b34
Mikhail Krasikov <corolish2@gmail.com>
2021-04-11 11:13:59 +0300

    v13-mysql

--- src/main/java/ru/mkrasikoff/springmvcapp/dao/PersonDAO.java
+++ src/main/java/ru/mkrasikoff/springmvcapp/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.mkrasikoff.springmvcapp.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.mkrasikoff.springmvcapp.models.Person;
@@ -19,18 +20,19 @@ public class PersonDAO {
     }
 
     public List<Person> showPeople() {
-        return jdbcTemplate.query("SELECT * FROM person", new PersonRowMapper());
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person showPerson(int id) {
-        return jdbcTemplate.query("SELECT * FROM person WHERE id = ?", new PersonRowMapper(), new Object[]{id})
+        return jdbcTemplate.query("SELECT * FROM person WHERE id = ?", new BeanPropertyRowMapper<>(Person.class),
+                new Object[]{id})
                 .stream().findAny().orElse(null);
     }
 
     public void savePerson(Person person) {
         Random random = new Random();
         jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)",
-               random.nextInt(128),person.getName(), person.getSurname(), person.getEmail());
+               random.nextInt(1024),person.getName(), person.getSurname(), person.getEmail());
     }
 
     public void updatePerson(Person updatedPerson, int id) {
