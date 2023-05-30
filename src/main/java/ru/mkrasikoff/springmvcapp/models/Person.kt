71499feb78e758ed4229ea3606b853d27ff0f9ed
Mikhail Krasikov <mikhail_krasikov@epam.com>
2023-05-30 13:35:19 +0300

    v19 - change entity from Java to Kotlin for Kotlin integration

---
+++ src/main/java/ru/mkrasikoff/springmvcapp/models/Person.kt
@@ -0,0 +1,21 @@
+package ru.mkrasikoff.springmvcapp.models
+
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Size
+
+data class Person(
+        var id: Int = 0,
+
+        @field:NotEmpty(message = "Name shouldn't be empty")
+        @field:Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+        var name: String? = null,
+
+        @field:NotEmpty(message = "Surname shouldn't be empty")
+        @field:Size(min = 2, max = 30, message = "Surname should be between 2 and 30 characters")
+        var surname: String? = null,
+
+        @field:NotEmpty(message = "Email shouldn't be empty")
+        @field:Email(message = "Email should be valid")
+        var email: String? = null
+)
