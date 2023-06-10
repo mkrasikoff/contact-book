1c5ca4d3c40d9f6024d1b8ed86dcb72dc00bf5fc
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-06-10 22:24:29 +0300

    v34 - added new integration tests

--- src/test/java/integration/JdbcPersonRepositoryIntegrationTest.java
+++ src/test/java/integration/JdbcPersonRepositoryIntegrationTest.java
@@ -10,9 +10,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.mkrasikoff.springmvcapp.models.Person;
 import ru.mkrasikoff.springmvcapp.repos.JdbcPersonRepository;
+import ru.mkrasikoff.springmvcapp.exceptions.PersonNotFoundException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = IntegrationTestConfig.class)
@@ -20,6 +25,10 @@ public class JdbcPersonRepositoryIntegrationTest {
 
     private static final String QUERY_INSERT_PERSON = "INSERT INTO person(id, name, surname, email) VALUES(?, ?, ?, ?)";
     private static final String QUERY_DELETE_PERSON = "DELETE FROM person";
+    private static final String MESSAGE_PERSON_NOT_FOUND = "Person with id 999 not found.";
+    private static final int ID_NONEXISTENT_USER = 999;
+
+    private Person person;
 
     @Autowired
     private JdbcPersonRepository personRepository;
@@ -28,25 +37,95 @@ public class JdbcPersonRepositoryIntegrationTest {
     private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
-    void clearPersonTable() {
+    void setup() {
         jdbcTemplate.update(QUERY_DELETE_PERSON);
+        person = createPerson();
+        jdbcTemplate.update(QUERY_INSERT_PERSON, person.getId(), person.getName(), person.getSurname(), person.getEmail());
     }
 
     @Test
-    public void givenPersonId_whenFindById_thenReturnCorrectPerson() {
-        Person person = createPerson();
+    public void findById_givenCorrectPersonId_personReturned() {
         int id = person.getId();
-        jdbcTemplate.update(QUERY_INSERT_PERSON,
-                person.getId(), person.getName(), person.getSurname(), person.getEmail());
 
         Person found = personRepository.findById(id);
 
         assertAll("Person",
-                () -> assertEquals(person.getId(), found.getId(), "ID should match"),
+                () -> assertEquals(id, found.getId(), "ID should match"),
                 () -> assertEquals(person.getName(), found.getName(), "Name should match"),
                 () -> assertEquals(person.getSurname(), found.getSurname(), "Surname should match"),
                 () -> assertEquals(person.getEmail(), found.getEmail(), "Email should match")
         );
+    }
+
+    @Test
+    public void findById_givenIncorrectPersonId_thrownException() {
+        PersonNotFoundException exception = assertThrows(PersonNotFoundException.class, () -> personRepository.findById(ID_NONEXISTENT_USER));
+
+        assertEquals(MESSAGE_PERSON_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void findAll_twoPersonsCreated_returnsAllPersons() {
+        Person secondPerson = new Person(2, "Eva", "Smith", "eva_smith@email.com");
+        jdbcTemplate.update(QUERY_INSERT_PERSON, secondPerson.getId(), secondPerson.getName(), secondPerson.getSurname(), secondPerson.getEmail());
+
+        List<Person> persons = personRepository.findAll();
+
+        assertEquals(2, persons.size());
+        assertTrue(persons.contains(person));
+        assertTrue(persons.contains(secondPerson));
+    }
+
+    @Test
+    public void save_givenValidPerson_returnsSavedPerson() {
+        Person newPerson = new Person(2, "Eva", "Smith", "eva_smith@email.com");
+
+        personRepository.save(newPerson);
+        List<Person> persons = personRepository.findAll();
+        Person savedPerson = persons.stream()
+                .filter(p -> p.getName().equals(newPerson.getName())
+                        && p.getSurname().equals(newPerson.getSurname())
+                        && p.getEmail().equals(newPerson.getEmail()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("Saved person not found"));
+
+        assertEquals(newPerson.getName(), savedPerson.getName(), "Names should match");
+        assertEquals(newPerson.getSurname(), savedPerson.getSurname(), "Surnames should match");
+        assertEquals(newPerson.getEmail(), savedPerson.getEmail(), "Emails should match");
+    }
+
+    @Test
+    public void update_givenExistingPerson_personUpdated() {
+        person.setName("Updated name");
+        personRepository.update(person, person.getId());
+        Person updatedPerson = personRepository.findById(person.getId());
+
+        assertEquals("Updated name", updatedPerson.getName());
+    }
+
+    @Test
+    public void update_givenNonExistingPerson_throwsException() {
+        Person nonExistingPerson = new Person(ID_NONEXISTENT_USER, "Non", "Existing", "non_existing@email.com");
+
+        PersonNotFoundException exception = assertThrows(PersonNotFoundException.class, () -> personRepository.update(nonExistingPerson, nonExistingPerson.getId()));
+
+        assertEquals(exception.getMessage(), MESSAGE_PERSON_NOT_FOUND);
+    }
+
+    @Test
+    public void delete_givenExistingPerson_personDeleted() {
+        assertNotNull(personRepository.findById(person.getId()));
+
+        personRepository.deleteById(person.getId());
+
+        assertThrows(PersonNotFoundException.class, () -> personRepository.findById(person.getId()));
+    }
+
+    @Test
+    public void delete_givenNonExistingPerson_throwsException() {
+        PersonNotFoundException exception = assertThrows(PersonNotFoundException.class, () -> personRepository.deleteById(ID_NONEXISTENT_USER));
+
+        assertEquals(exception.getMessage(), MESSAGE_PERSON_NOT_FOUND);
     }
 
     private Person createPerson() {
