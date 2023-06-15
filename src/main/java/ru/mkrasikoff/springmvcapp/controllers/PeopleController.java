b11c27f73849c4a35d5ce9418995229e741c211f
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-06-15 17:23:13 +0300

    v41 - modify PeopleController to make it more suitable to REST conventions, documentation added

--- src/main/java/ru/mkrasikoff/springmvcapp/controllers/PeopleController.java
+++ src/main/java/ru/mkrasikoff/springmvcapp/controllers/PeopleController.java
@@ -19,73 +19,142 @@ public class PeopleController {
         this.personService = personService;
     }
 
-    @GetMapping("/show")
-    public String showPeople(Model model) {
+    /**
+     * Display a list of all people.
+     *
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
+    @GetMapping
+    public String getPeople(Model model) {
         model.addAttribute("people", personService.showPeople());
         return "people/showPeople";
     }
 
-    @GetMapping("/show/{id}")
-    public String showPerson(@PathVariable("id") int id, Model model) {
+    /**
+     * Display a single person based on his ID.
+     *
+     * @param id The ID of the person to display.
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
+    @GetMapping("/{id}")
+    public String getPerson(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personService.showPerson(id));
         return "people/showPerson";
     }
 
-    @GetMapping("/new")
+    /**
+     * Provide the form to create a new person.
+     *
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
+    @GetMapping("/create")
     public String newPerson(Model model) {
         model.addAttribute("person", new Person());
         return "people/newPerson";
     }
 
+    /**
+     * Handle the submission of the form to create a new person.
+     *
+     * @param person The person to create.
+     * @param bindingResult The result of the form binding.
+     * @return The view to display.
+     */
     @PostMapping
-    public String createPerson(@ModelAttribute("person") @Valid Person person,
-                               BindingResult bindingResult) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) return "people/newPerson";
         personService.savePerson(person);
-        return "redirect:/people/show";
+        return "redirect:/people";
     }
 
+    /**
+     * Display the form for updating all people.
+     *
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
     @GetMapping("/edit")
-    public String editPeople(Model model) {
+    public String editAll(Model model) {
         model.addAttribute("people", personService.showPeople());
         return "people/editPeople";
     }
 
+    /**
+     * Display the form for updating a single person based on his ID.
+     *
+     * @param id The ID of the person to update.
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
     @GetMapping("/{id}/edit")
-    public String editPerson(Model model,
-                             @PathVariable("id") int id) {
+    public String editOne(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personService.showPerson(id));
         return "people/editPerson";
     }
 
+    /**
+     * Handle the submission of the form to update a person.
+     *
+     * @param person The person to update.
+     * @param bindingResult The result of the form binding.
+     * @param id The ID of the person to update.
+     * @return The view to display.
+     */
     @PatchMapping("/{id}")
-    public String updatePerson(@ModelAttribute("person") @Valid Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
                                BindingResult bindingResult,
                                @PathVariable("id") int id) {
         if(bindingResult.hasErrors()) return "people/editPerson";
         personService.updatePerson(person, id);
-        return "redirect:/people/show";
+        return "redirect:/people";
     }
 
+    /**
+     * Display the form for all deletable people.
+     *
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
     @GetMapping("/delete")
-    public String deletePeople(Model model) {
+    public String getDeletablePeople(Model model) {
         model.addAttribute("people", personService.showPeople());
         return "people/deletePeople";
     }
 
+    /**
+     * Display the confirmation page for deleting a person.
+     *
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
     @GetMapping("/{id}/delete")
-    public String deletePerson(Model model,
-                               @PathVariable("id") int id) {
+    public String getDeletablePerson(Model model, @PathVariable("id") int id) {
         model.addAttribute("person",personService.showPerson(id));
         return "people/deletePersonConfirm";
     }
 
+    /**
+     * Handle the confirmation to delete a person.
+     *
+     * @param id The ID of the person to delete.
+     * @return The view to display.
+     */
     @DeleteMapping("/{id}")
-    public String removePerson(@PathVariable("id") int id) {
+    public String delete(@PathVariable("id") int id) {
         personService.deletePerson(id);
-        return "redirect:/people/show";
+        return "redirect:/people";
     }
 
+    /**
+     * Display people based on a search query.
+     *
+     * @param query The search query.
+     * @param model The Model object to bind data to the view.
+     * @return The view to display.
+     */
     @GetMapping("/search")
     public String search(@RequestParam String query, Model model) {
         List<Person> persons = personService.search(query);
@@ -93,20 +162,35 @@ public class PeopleController {
         return "people/showPeople";
     }
 
-    @GetMapping("/generate")
+    /**
+     * Generate a list of random people.
+     *
+     * @return The view to display.
+     */
+    @PostMapping("/generate")
     public String generateRandomPeople() {
         personService.createRandomPeople();
-        return "redirect:/people/show";
+        return "redirect:/people";
     }
 
+    /**
+     * Display the confirmation page for deleting all people.
+     *
+     * @return The view to display.
+     */
     @GetMapping("/deleteAll/confirm")
     public String confirmDeleteAll() {
         return "people/deletePeopleConfirm";
     }
 
+    /**
+     * Handle the confirmation to delete all people.
+     *
+     * @return The view to display next.
+     */
     @DeleteMapping("/deleteAll")
     public String deleteAll() {
         personService.deleteAllPeople();
-        return "redirect:/people/show";
+        return "redirect:/people";
     }
 }
