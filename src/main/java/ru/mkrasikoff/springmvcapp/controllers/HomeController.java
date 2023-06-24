bce031f8235752e2762fe9eca2c2854a98aa25d5
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-06-24 21:49:19 +0300

    v51 - changed homePage style, added app icon to browser tab

--- src/main/java/ru/mkrasikoff/springmvcapp/controllers/HomeController.java
+++ src/main/java/ru/mkrasikoff/springmvcapp/controllers/HomeController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping("/")
     public String welcomePage() {
-        return "/main/welcomePage";
+        return "/main/homePage";
     }
 }
